50edf3de5c1f0b7969c07192d9feec088a3c8734
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2024-06-29 12:37:46 +0600

    your hobby your communit component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import AddYourOwn from "@/components/ui/AddYourOwn/AddYourOwn";
 import HobbyResources from "@/components/ui/HobbyResources/HobbyResources";
+import YourHobby from "@/components/ui/YourHobby/YourHobby";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       {/* <Navbar></Navbar> */}
       <HobbyResources></HobbyResources>
       <AddYourOwn></AddYourOwn>
+      <YourHobby></YourHobby>
       <Footer></Footer>
     </main>
   );
---
+++ src/components/ui/YourHobby/YourHobby.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Container from "../Container";
+import hobbyImage from "../../../../public/assests/your_hobby_img.svg";
+import Image from "next/image";
+
+const YourHobby = () => {
+  return (
+    <section className="bg-[#F7FDFF] pt-14 pb-5">
+      <Container>
+        <div>
+          <h2 className="italic text-lg md:text-4xl font-normal">
+            {" "}
+            Your <span className="text-[#8064A2]">Hobby</span>, Your{" "}
+            <span className="text-[#0096C8]">Community ...</span>{" "}
+          </h2>
+
+          <button className="text-sm md:text-xl bg-[#8064A2] font-semibold text-white px-4 py-2 rounded-lg mt-5">
+            Get Started
+          </button>
+        </div>
+
+        <div className="mt-14">
+          <Image
+            src={hobbyImage}
+            style={{ width: "100%", height: "25%" }}
+            alt="hobby_image"
+          ></Image>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default YourHobby;
