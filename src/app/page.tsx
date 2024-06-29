99c07a914f410015141a096d5f410eecdcadb2dc
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2024-06-29 11:03:59 +0600

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {/* <Navbar></Navbar> */}
       <HobbyResources></HobbyResources>
       <AddYourOwn></AddYourOwn>
-      {/* <Footer></Footer> */}
+      <Footer></Footer>
     </main>
   );
 }
---
+++ src/components/shared/SubFooter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubFooter = () => {
+  return (
+    <div>
+      <h3>This is sub footer</h3>
+    </div>
+  );
+};
+
+export default SubFooter;
